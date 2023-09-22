0a95817d90b2a7f87e10948d0a86866628153fbd
sathyat20 <sathya.thenappan.23@dartmouth.edu>
2023-09-22 18:15:15 +0800

    All files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
 import React from "react";
-import logo from "./logo.png";
+import StartPage from "./Pages/Start/StartPage.js";
+import QuizPage from "./Pages/Quiz/QuizPage.js";
+import ResultPage from "./Pages/Result/ResultPage.js"
+import data from "./db.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: {},
+      currentPage: "start",
+      data: null,
+    };
+  }
+
+  startQuiz = () => {
+    this.setState({
+      currentPage: "quiz",
+    });
+  };
+
+  restartQuiz = () => {
+    this.setState({
+      currentPage: "start",
+      answers: {}
+    });
+  };
+
+  submitAnswers = (answers) => {
+    this.setState({
+      answers: answers,
+      currentPage: "result",
+    });
+  };
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      // Start page
+      <div className="container">
+        {this.state.currentPage === "start" && (
+          <StartPage startQuiz={this.startQuiz} />
+        )}
+
+        {this.state.currentPage === "quiz" && (
+          <QuizPage
+            questions={data.questions}
+            submitAnswers={this.submitAnswers}
+          />
+        )}
+
+        {this.state.currentPage === "result" && (
+          <ResultPage
+            answers={this.state.answers}
+            combinations={data.combinations}
+            restart={this.restartQuiz}
+          />
+        )}
       </div>
-    );
+    )
   }
 }
 
